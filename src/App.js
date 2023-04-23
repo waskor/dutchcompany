0020d279ee4918d794d12c0df166d3cef57c05f7
waskor <98026708+waskor@users.noreply.github.com>
2023-04-23 16:38:27 +0200

    add catalog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
-    <div className="body-color-transition" style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
-      <img src="/dutch_cones.png" style={{width: "50%"}}/>
+    <div
+      className="body-color-transition"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        position: "relative",
+      }}
+    >
+      <div>
+        <a href="/DUTCH_COMPANY.pdf" download="DUTCH_COMPANY.pdf">
+          <img
+            src="/dutch_cones.png"
+            style={{ width: "50vw" }}
+            className="logo"
+          />
+        </a>
+      </div>
     </div>
   );
 }
