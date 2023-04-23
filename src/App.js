48e5cd532f89dd1779d973a97747fd66c8abc14b
waskor <98026708+waskor@users.noreply.github.com>
2023-04-23 15:26:11 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import "./App.css"
+
+function App() {
+  return (
+    <div className="body-color-transition" style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      <img src="/dutch_cones.png" style={{width: "50%"}}/>
+    </div>
+  );
+}
+
+export default App;
